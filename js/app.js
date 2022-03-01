ba22520e27a0631e6841795bae397710441118ab
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-03-01 09:39:34 +0600

    fetch data from api

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
+// get allPhone data
 const allPhone = () => {
-  const searchValue = document.getElementById("input-box").value;
-  console.log(searchValue);
+  const searchValue = document.getElementById("search-box").value;
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((phone) => console.log(phone.data));
+  console.log(url);
 };
